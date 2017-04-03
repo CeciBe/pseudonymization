ce5ca1c387b039ca45cad0b2d09241339d23084a
Cecilia Bergman <cili.bergman@gmail.com>
2017-04-03 14:32:11 +0100

    Lagt till Parser klass

--- src/main/java/pseudo/Parser.java
+++ src/main/java/pseudo/Parser.java
@@ -1,4 +1,8 @@
 package pseudo;
 
+
 public class Parser {
+
+
+
 }
--- src/main/java/pseudo/Program.java
+++ src/main/java/pseudo/Program.java
@@ -3,9 +3,14 @@ package pseudo;
 /**
  * Created by Cecilia Bergman and Anna Schuback.
  */
+
+
 public class Program {
 
     public static void main(String[] args){
+
+        System.out.println("Hello world!");
+
 
     }
 }
