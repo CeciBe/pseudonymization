983ef0d72f93ec41c661545e5c8e3d51eb808e97
Cecilia Bergman <cili.bergman@gmail.com>
2017-04-05 13:43:27 +0200

    Lagt till Scanner

--- src/main/java/pseudo/Program.java
+++ src/main/java/pseudo/Program.java
@@ -1,8 +1,6 @@
 package pseudo;
 
 
-
-
 public class Program {
 
     public static void main(String[] args){
--- src/main/java/pseudo/Scanner.java
+++ src/main/java/pseudo/Scanner.java
@@ -1,5 +1,45 @@
 package pseudo;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 
 public class Scanner {
+
+    public static final char NULL = (char)0;
+    public static final char EOF = (char)-1; // End of file.
+
+    private FileInputStream stream = null;
+    private InputStreamReader reader = null;
+    private char current = NULL;
+
+    public Scanner() {
+    }
+
+    public void open(String fileName) throws FileNotFoundException {
+        stream = new FileInputStream(fileName);
+        reader = new InputStreamReader(stream);
+    }
+
+    public char current() {
+        return current;
+    }
+
+    public void moveNext() throws IOException {
+        if (reader == null)
+            throw new IOException("No open file.");
+        if (current != EOF)
+            current = (char)reader.read();
+    }
+
+    public void close() throws IOException {
+        if (reader != null)
+            reader.close();
+        if (stream != null)
+            stream.close();
+    }
+
+
 }
