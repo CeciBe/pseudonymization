59100889fbab0e54cadb3cfeb00e0e6c4cc62834
Cecilia Bergman <cili.bergman@gmail.com>
2017-04-10 11:41:00 +0200

    Lagt till StringBuilder och FileOutputStream

--- src/main/java/pseudo/Program.java
+++ src/main/java/pseudo/Program.java
@@ -2,6 +2,8 @@ package pseudo;
 
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 public class Program {
 
@@ -9,13 +11,28 @@ public class Program {
         String inputFile = "C:/inputData.txt";
         String outputFile = "C:/outputData.txt";
 
+        StringBuilder builder = null;
+        FileOutputStream stream = null;
+        OutputStreamWriter writer = null;
+
         //Behövs StringBuilder för att bygga texten? Antagligen (om inget annat alternativ är bättre).
 
         Parser parser = new Parser();
         try {
-            parser.openFile(inputFile);
-        } catch (FileNotFoundException fileEx) {
-            System.err.print("Couldn't open file");
+            try {
+                parser.openFile(inputFile);
+                builder = new StringBuilder();
+
+                stream = new FileOutputStream(outputFile);
+                writer = new OutputStreamWriter(stream);
+                writer.write(builder.toString());
+
+
+            } catch (FileNotFoundException fileEx) {
+                System.err.print("Couldn't open file " + fileEx.getMessage());
+            }
+        }catch (Exception exception){
+            System.err.print("Exception: " + exception.getMessage());
         }
 
     }
