b778021f5795c1b414e66d219bb5343d73071772
Anna <anna.schuback@gmail.com>
2017-04-24 13:31:08 +0200

    Testar för min egen branch

--- src/main/java/pseudo/Program.java
+++ src/main/java/pseudo/Program.java
@@ -1,70 +1,19 @@
 package pseudo;
 
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.util.*;
+import java.io.*;
 import java.nio.channels.FileChannel;
 
 public class Program {
 
+    private Scanner scan = null;
+
+    public Program() {
+
+    }
+
     public static void main(String[] args) {
-        String inputFile = "C:/inputData.txt";
-        String outputFile = "C:/outputData.txt";
 
-        //StringBuilder builder = null;
-        //FileOutputStream stream = null;
-        //OutputStreamWriter writer = null;
-
-        //Behövs StringBuilder för att bygga texten? Antagligen (om inget annat alternativ är bättre).
-
-        //Parser parser = null; //new Parser();
-        //Scanner scan = null;
-
-        FileChannel sourceChannel = null;
-        FileChannel destChannel = null;
-
-        try {
-            try {
-                //scan = new Scanner();
-                //parser = new Parser();
-                //parser.openFile(inputFile);
-                //builder = new StringBuilder();
-
-                //builder.append(inputFile);
-
-                //stream = new FileOutputStream(outputFile);
-                //writer = new OutputStreamWriter(stream);
-                //writer.write(builder.toString());
-
-                sourceChannel = new FileInputStream(inputFile).getChannel();
-                destChannel = new FileOutputStream(outputFile).getChannel();
-                destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
-
-            } catch (FileNotFoundException fileEx) {
-                System.err.print("Couldn't open file " + fileEx.getMessage());
-            }
-            finally {
-                /*if (parser != null)
-                    parser.close();
-                if (scan != null)
-                    scan.close();
-                if (writer != null)
-                    writer.close();
-                if (stream != null)
-                    stream.close();*/
-                if (sourceChannel != null) {
-                    sourceChannel.close();
-                }
-                if (destChannel != null) {
-                    destChannel.close();
-                }
-            }
-
-        }catch (Exception exception){
-            System.err.print("Exception: " + exception.getMessage());
-        }
 
     }
 }
