65564605797697b7af808a125a56a7b5951e46a4
Cecilia Bergman <cili.bergman@gmail.com>
2017-05-05 15:02:28 +0200

    Pattern Matcher tillagt och byter nu ut alla HCU taggar till SÖS och alla Location taggar till Stockholm.

--- src/main/java/pseudo/Program.java
+++ src/main/java/pseudo/Program.java
@@ -1,43 +1,82 @@
 package pseudo;
 
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.nio.channels.FileChannel;
+import java.util.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.valueOf;
+
+/**OBS! Nedanstående två rader ska vi avvakta med då det har med weka att göra!**/
+//import weka.core.Instances;         //För datainsamlingen
+//import weka.filters.Filter;         //För att preprocessa data
+
 
 public class Program {
 
-    public static void main(String[] args) {
+    private Scanner scan = null;
+    private boolean isRunning;
+
+
+    public Program() {
+        scan = new Scanner(System.in);
+    }
+
+    public void run(boolean condition) {
+        isRunning = condition;
+        System.out.println(initiate());
+        while(isRunning) {
+            System.out.println(printMenu());
+            switch(commando()) {
+                case "1":
+                    copyData();
+                    break;
+                case "2":
+                    pseudonymize();
+                    //System.out.println("Maintenance work, please return later!\n");
+                    break;
+                case "3":
+                    isRunning = false; System.out.println("The program is closing"); closeScanner();
+                    return;
+                case "4":
+                    break;
+                default:
+                    System.out.println("The option is not valid try again!\n");
+            }
+        }
+    }
+
+    public String readText(String text) {
+        System.out.print(text);
+        return scan.nextLine();
+    }
+
+    public String commando() {
+        return readText("> ").toLowerCase();
+    }
+
+    public String initiate() {
+        return "The program is initiated\n";
+    }
+
+    public String printMenu() {
+        return "1. Copy EPR data \n2. Initiate pseudonymization \n3. Close program";
+    }
+
+
+
+    public void copyData() {
+
         String inputFile = "C:/inputData.txt";
         String outputFile = "C:/outputData.txt";
-
-        //StringBuilder builder = null;
-        //FileOutputStream stream = null;
-        //OutputStreamWriter writer = null;
-
-        //Behövs StringBuilder för att bygga texten? Antagligen (om inget annat alternativ är bättre).
-
-        //Parser parser = null; //new Parser();
-        //Scanner scan = null;
 
         FileChannel sourceChannel = null;
         FileChannel destChannel = null;
 
         try {
             try {
-                //scan = new Scanner();
-                //parser = new Parser();
-                //parser.openFile(inputFile);
-                //builder = new StringBuilder();
-
-                //builder.append(inputFile);
-
-                //stream = new FileOutputStream(outputFile);
-                //writer = new OutputStreamWriter(stream);
-                //writer.write(builder.toString());
-
                 sourceChannel = new FileInputStream(inputFile).getChannel();
                 destChannel = new FileOutputStream(outputFile).getChannel();
                 destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
@@ -46,14 +85,6 @@ public class Program {
                 System.err.print("Couldn't open file " + fileEx.getMessage());
             }
             finally {
-                /*if (parser != null)
-                    parser.close();
-                if (scan != null)
-                    scan.close();
-                if (writer != null)
-                    writer.close();
-                if (stream != null)
-                    stream.close();*/
                 if (sourceChannel != null) {
                     sourceChannel.close();
                 }
@@ -65,6 +96,68 @@ public class Program {
         }catch (Exception exception){
             System.err.print("Exception: " + exception.getMessage());
         }
+        System.out.println("\nA copy of the file is created!\n");
+    }
+
+
+    public void pseudonymize(){
+
+        try {
+
+            String verify;
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:/outputData.txt"), "ISO-8859-1"));
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:/newFile.txt"),"ISO-8859-1"));
+
+
+            while ((verify = reader.readLine()) != null){
+
+                String patternString1 = "<Health_Care_Unit>\\s*(.+?)\\s*</Health_Care_Unit>";
+                String patternString2 = "<Location>\\s*(.+?)\\s*</Location>";
+
+                Pattern pattern1 = Pattern.compile(patternString1);
+                Matcher matcher1 = pattern1.matcher(verify);
+
+                Pattern pattern2 = Pattern.compile(patternString2);
+                Matcher matcher2 = pattern2.matcher(verify);
+
+                //Loopen räknar bara ut var taggarna finns och är inte viktigt för programmet
+                //lät den vara kvar tills vidare.
+                int count = 0;
+                while(matcher1.find() && matcher2.find()) {
+                    count++;
+                    System.out.println("found Location: " + count + " : "
+                            + matcher2.start() + " - " + matcher2.end()
+                            + "and HCU: " + matcher1.start() + " - " + matcher1.end());
+
+                }
+
+                writer.write(verify.replaceAll(patternString2, "<Location>Stockholm</Location>").replaceAll(patternString1, "<Health_Care_Unit>SÖS</Health_Care_Unit>"));
+
+
+            }
+            reader.close();
+            writer.close();
+
+
+        }
+        catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+    public void closeScanner() {
+        if(scan != null) {
+            scan.close();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Program program = new Program();
+        program.run(true);
 
     }
 }
