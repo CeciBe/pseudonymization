043e8585180674c02b0aa501f6b1df524a578361
Cecilia Bergman <cili.bergman@gmail.com>
2017-05-05 14:18:24 +0200

    Pattern Matcher byter nu ut alla Location taggar till Stockholm.

--- src/main/java/pseudo/Program.java
+++ src/main/java/pseudo/Program.java
@@ -2,7 +2,12 @@ package pseudo;
 
 import java.io.*;
 import java.nio.channels.FileChannel;
+import java.util.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.valueOf;
 
 /**OBS! Nedanstående två rader ska vi avvakta med då det har med weka att göra!**/
 //import weka.core.Instances;         //För datainsamlingen
@@ -13,6 +18,8 @@ public class Program {
 
     private Scanner scan = null;
     private boolean isRunning;
+    private ArrayList<String> list = new ArrayList<>();
+
 
     public Program() {
         scan = new Scanner(System.in);
@@ -34,6 +41,14 @@ public class Program {
                 case "3":
                     isRunning = false; System.out.println("The program is closing"); closeScanner();
                     return;
+                case "4":
+                    for(String s : list) {
+                        //System.out.println("Så många strängar i listan: " + list.size());
+                        System.out.println(s);
+                    }
+                    System.out.println("Så många strängar i listan: " + list.size());
+
+                    break;
                 default:
                     System.out.println("The option is not valid try again!\n");
             }
@@ -94,29 +109,150 @@ public class Program {
 
     public void pseudonymize(){
 
-       try {
-           String verify, put1, put2;
+        try {
 
-           BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:/outputData.txt"), "ISO-8859-1"));
+            //HashMap<String, String> map = new HashMap<>();
 
-           BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:/newFile.txt"),"ISO-8859-1"));
+            String verify, put1, put2, op;
 
-           // Always wrap FileReader in BufferedReader.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:/outputData.txt"), "ISO-8859-1"));
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:/newFile.txt"),"ISO-8859-1"));
+
 
             while ((verify = reader.readLine()) != null){
+                int numberOfTokens = 0;
+                StringTokenizer st = new StringTokenizer(verify);
+                StringBuilder sb = new StringBuilder();
+                String currentToken = "";
+                System.out.println("Före: " + verify);
 
-                if (verify.contains("Location")) {
-                    put1 = verify.replaceAll("Location", "L");
-                    writer.write(put1);
-                }
-                if (verify.contains("Health_Care_Unit")){
-                    put2 = verify.replaceAll("Health_Care_Unit", "H");
-                    writer.write(put2);
+                System.out.println("Efter: ");
+
+                //while (st.hasMoreTokens()) {
+                    //currentToken = st.nextToken();
+
+
+                    String patternString = "<Location>\\s*(.+?)\\s*</Location>";
+
+                    Pattern pattern = Pattern.compile(patternString);
+                    Matcher matcher = pattern.matcher(verify);
+
+                    int count = 0;
+                    while(matcher.find()) {
+                        count++;
+                        System.out.println("found: " + count + " : "
+                                + matcher.start() + " - " + matcher.end());
+
                     }
 
+                    writer.write(verify.replaceAll(patternString, "<Location>Stockholm</Location>"));
+
+
+                    /*if (currentToken.contains("<Location>")) {
+                        System.out.println("Nuvarande token: " + currentToken);
+                        //put1 = String.valueOf(sb.append(currentToken).append(" "));
+
+                        if (currentToken.contains("</Location>")) {
+                            //list.add(currentToken);
+                            put1 = valueOf(sb.append(currentToken).append("-"));
+                            //list.add(put1);
+                            //writer.write(verify.replaceAll(put1, "<Location>Stockholm</Location>"));
+                            System.out.println("Nuvarande token i listan: " + currentToken);
+                            //continue;
+                        } else {
+                            put1 = valueOf(sb.append(currentToken).append(" "));
+                            currentToken = st.nextToken();
+                            if (currentToken.contains("</Location>")) {
+                                put1 = valueOf(sb.append(currentToken).append("-"));
+                                //list.add(put1);
+                                //writer.write(verify.replaceAll(put1, "<Location>Stockholm</Location>"));
+                                System.out.println("Nuvarande kortare sträng: " + put1);
+                                //continue;
+
+                                for(String s : put1.split("-")) {
+                                    System.out.println("Splittad sträng: " + s);
+                                    list.add(s.trim());
+                                    //writer.write(verify.replaceAll(s, "<Location>Stockholm</Location>"));
+                                }
+                            } else {
+                                while (!currentToken.contains("</Location>")) {
+                                    put1 = valueOf(sb.append(currentToken).append(" "));
+                                    currentToken = st.nextToken();
+
+                                    if (currentToken.contains("</Location>")) {
+                                        put1 = valueOf(sb.append(currentToken).append("-"));
+                                        //list.add(put1);
+                                        //writer.write(verify.replaceAll(put1, "<Location>Stockholm</Location>"));
+                                        //currentToken = st.nextToken();
+                                        //put1 = String.valueOf(sb.append(currentToken).append(" "));
+                                        System.out.println("Nuvarande sträng om inte är slut: " + put1);
+
+                                        //continue;
+                                        //} else {
+                                        //put1 = String.valueOf(sb.append(currentToken).append(" "));
+                                        //list.add(put1);
+
+                                        //System.out.println("Nuvarande sträng om inte är slut: " + put1);
+                                        //map.put(verify);
+                                        //list.spliterator();
+
+
+
+                                    }
+                                }
+                            }*/
+                            //put1 = verify.replaceAll(String.valueOf(sb.append("<Location>" + "</Location>")), String.valueOf(sb.append("<Location>" + "Stockholm" + "</Location>")));
+                            //put1 = verify.replaceAll("Location", "L");
+                           // writer.write(verify.replaceAll(put1, "<Location>Stockholm</Location> "));
+                            //currentToken = st.nextToken();
+                            //}
+                            //String[] items = put1.split("> <");
+                            //List<String> list = Arrays.asList(items);
+
+                            //System.out.println(list);
+                            //for(String s : put1.split("> <")) {
+                                //System.out.println("Splittad sträng: " + s);
+                            //}
+                           /*for(String s : put1.split("-")) {
+                                System.out.println("Splittad sträng: " + s);
+                                list.add(s.trim());
+                                //writer.write(verify.replaceAll(s, "<Location>Stockholm</Location>"));
+                            }*/
+
+                        //}
+                        //writer.write(verify.replaceAll(put1, "<Location>Stockholm</Location>"));
+                        //list.add(put1);
+                        //System.out.println("Listan innehåller: " + put1.length());
+                        //verify.replaceAll(put1, "<Location>Stockholm</Location>");
+                        //writer.write(verify.replaceAll(String.valueOf(list), "<Location>Stockholm</Location>"));
+                      /*  for(String s : put1.split("-")) {
+                            System.out.println("Splittad sträng: " + s);
+                            list.add(s.trim());
+                            writer.write(verify.replaceAll(s, "<Location>Stockholm</Location>"));
+
+                        }*/
+                    //}
+
+
+                    /*if (verify.contains("<Health_Care_Unit>")) { //&& verify.endsWith("</Health_Care_Unit>")){
+                        //list.add(verify);
+                        put2 = verify.replaceAll("Health_Care_Unit", "H");
+                        writer.write(put2);
+
+                    }*/
+                    //writer.write(verify);
+                    //writer.write(verify.replaceAll(String.valueOf(verify.contains("<Location>")), "<Location>Stockholm</Location>"));
+
+                    //numberOfTokens++;
+                    //writer.write(verify.replaceAll(String.valueOf(verify.contains("<Location>")), "<Location>Stockholm</Location>"));
+
+                //}
+                System.out.println("Total Tokens: "+ numberOfTokens);
+
             }
-           reader.close();
-           writer.close();
+            reader.close();
+            writer.close();
 
 
         }
