baa057a89a36ad61d990cf0e23929e242562833a
Anna <anna.schuback@gmail.com>
2017-05-09 20:53:47 +0200

    Håller på att koppla ihop värde med surrogat.

--- src/main/java/pseudo/Program.java
+++ src/main/java/pseudo/Program.java
@@ -32,9 +32,12 @@ public class Program {
                     copyData();
                     break;
                 case "2":
-                    pseudonymize();
+                    pseudonymizer.createLists();
                     break;
                 case "3":
+                    pseudonymize();
+                    break;
+                case "4":
                     isRunning = false; System.out.println("The program is closing"); closeScanner();
                     return;
                 default:
@@ -57,7 +60,7 @@ public class Program {
     }
 
     public String printMenu() {
-        return "1. Copy EPR data \n2. Initiate pseudonymization \n3. Close program";
+        return "1. Copy EPR data \n2. Fill lists \n3. Initiate pseudonymization \n4. Close program";
     }
 
 
@@ -95,14 +98,14 @@ public class Program {
     }
 
 
-    public void pseudonymize(){
-
+    public void pseudonymize() {
         try {
 
             String verify;
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:/outputData.txt"), "ISO-8859-1"));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:/newFile.txt"),"ISO-8859-1"));
+            //BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:/newFile.txt"),"ISO-8859-1"));
+
 
             while ((verify = reader.readLine()) != null){
 
@@ -115,34 +118,20 @@ public class Program {
                 Pattern pattern2 = Pattern.compile(patternString2);
                 Matcher matcher2 = pattern2.matcher(verify);
 
+
                 //TODO, angående nedanstående loopar, vi måste se till att innehållet som hämtas ska hanteras på något sätt
+
                 while (matcher1.find()) {
                     String h_c_u_Unit = matcher1.group(1);
-                    System.out.println(h_c_u_Unit);  //Testar utskrift av H_C_U
+                    pseudonymizer.pseudonymizeData(h_c_u_Unit, "Health_Care_Unit");
                 }
 
-               while (matcher2.find()) {
+                while (matcher2.find()) {
                     String locationUnit = matcher2.group(1);
-                    System.out.println(locationUnit);  //Testar utskrift av Location
+                    pseudonymizer.pseudonymizeData(locationUnit, "Location");
                 }
-
-                //Loopen räknar bara ut var taggarna finns och är inte viktigt för programmet
-                //lät den vara kvar tills vidare.
-
-                int count = 0;
-                while (matcher1.find() && matcher2.find()) {
-                    count++;
-                    System.out.println("found Location: " + count + " : "
-                            + matcher2.start() + " - " + matcher2.end()
-                            + "and HCU: " + matcher1.start() + " - " + matcher1.end());
-                }
-
-                writer.write(verify.replaceAll(patternString2, "<Location>Stockholm</Location>").replaceAll(patternString1, "<Health_Care_Unit>SÖS</Health_Care_Unit>"));
-
             }
             reader.close();
-            writer.close();
-
         }
         catch(IOException e) {
             System.err.println(e.getMessage());
--- src/main/java/pseudo/Pseudonymizer.java
+++ src/main/java/pseudo/Pseudonymizer.java
@@ -1,7 +1,208 @@
 package pseudo;
 
+import java.io.*;
+import java.util.*;
 
 public class Pseudonymizer {
 
+    private String locationListLink = "C:/LocationList.txt";
+    private String hcuListLink = "C:/HCUList.txt";
+    private TempData tempObject = new TempData();
+
+    private ArrayList <HashMap <String,String>> locationList = new ArrayList<>();
+    private ArrayList <HashMap <String,String>> hcuList = new ArrayList<>();
+    private HashMap <String, String> pseudonymizedData = new HashMap<>();
+
+    //private ArrayList <String> listOfDefalutValues = new ArrayList<>();     //För värden som inte går att kategorisera
+    //private HashMap <String, String> newDefaultValues = new HashMap<>();
+
+
     public Pseudonymizer() {}
+
+
+    public void createLists() {
+        BufferedReader locationReader = null;
+        String line;
+        try {
+            locationReader = new BufferedReader(new InputStreamReader(new FileInputStream(locationListLink), "ISO-8859-1"));
+            while((line = locationReader.readLine()) != null) {
+                HashMap <String,String> tempMap = new HashMap<>();
+                String category = line;
+                while(((line = locationReader.readLine()) != null) && !(line.equals("LOCATIONTYPE:"))) {
+                    String input = line;
+                    tempMap.put(input, category);
+                }
+                locationList.add(tempMap);
+            }
+            locationReader.close();
+        } catch (IOException ioEx) {
+            System.err.println(ioEx.getMessage());
+        }
+        continiueCreatingLists();
+        System.out.println("\nNew lists are created!\n");
+    }
+
+    public void continiueCreatingLists() {
+        BufferedReader hcuReader = null;
+        String line;
+        try {
+            hcuReader = new BufferedReader(new InputStreamReader(new FileInputStream(hcuListLink), "ISO-8859-1"));
+            while((line = hcuReader.readLine()) != null) {
+                HashMap <String,String> tempMap = new HashMap<>();
+                String category = line;
+                while(((line = hcuReader.readLine()) != null) && !(line.equals("HCUTYPE:"))) {
+                    String input = line;
+                    tempMap.put(input, category);
+                }
+                hcuList.add(tempMap);
+            }
+            hcuReader.close();
+        } catch (IOException ioEx) {
+            System.err.println(ioEx.getMessage());
+        }
+    }
+
+
+    public void pseudonymizeData(String inputData, String category) {
+
+        boolean isFound = false;
+        HashMap<String, String> tempMap = null;
+        if(category.equals("Location")) {
+            for (int index = 0; index < locationList.size(); index++) {
+                tempMap = locationList.get(index);
+                String categoryText = tempMap.get(inputData);
+                if(categoryText != null) {
+                    index = locationList.size();
+                    isFound = true;
+                }
+            }
+        } else if(category.equals("Health_Care_Unit")) {
+            for(int index = 0; index < hcuList.size(); index++) {
+                tempMap = hcuList.get(index);
+                String categoryText = tempMap.get(inputData);
+                if(categoryText != null) {
+                    index = locationList.size();
+                    isFound = true;
+                }
+            }
+        }
+        if (isFound == true) {
+            continiuePseudonymization(inputData,category, tempMap);
+        }
+    }
+
+    public void continiuePseudonymization(String inputData, String category, HashMap<String, String> tempMap) {
+        ArrayList <String> newList = null;
+        if(category.equals("Location")) {
+            newList = tempObject.getTemporaryList(tempMap);
+        } else if(category.equals("Health_Care_Unit")) {
+            newList = tempObject.getTemporaryList(tempMap);
+        }
+        generateSurrogate(inputData,newList);
+    }
+
+    public void generateSurrogate(String inputData, ArrayList <String> newList) {
+
+        System.out.println(inputData + " " + newList);
+    }
+
+    class TempData {
+
+        TempData() {
+        }
+
+        public ArrayList<String> getTemporaryList(HashMap<String, String> tempMap) {
+            ArrayList<String> tempList = new ArrayList<>();
+            for(Map.Entry<String, String> iter: tempMap.entrySet()) {
+                    String key = iter.getKey();
+                    tempList.add(key);
+            }
+            return tempList;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Nedanstående text används ej för tillfället
+
+
+
+    /*private HashMap <Character, ArrayList<String>> länder = new HashMap<>();
+    private HashMap <Character, ArrayList<String>> landskap = new HashMap<>();
+    private HashMap <Character, ArrayList<String>> landsDelar = new HashMap<>();
+    private HashMap <Character, ArrayList<String>> län = new HashMap<>();*/
+
+
+
+/*** ArrayList<String> tempList = LocationMap.get(firstChar);
+ if(tempList == null) {
+ tempList = new ArrayList<>();
+ tempList.add(line);
+ //LocationMap.put(firstChar, tempList);
+ } else {
+ tempList.add(line);
+ }***/
+
+
+
+/*public void createLists() {
+        BufferedReader locationReader = null;
+        String line; String currentCategory; int counter = 0;
+        try {
+            locationReader = new BufferedReader(new InputStreamReader(new FileInputStream(locationListLink), "ISO-8859-1"));
+
+            while(((line = locationReader.readLine()) != null) && !(line.equals("LOCATIONTYPE:")))  {
+                char firstChar = line.charAt(0);
+                ArrayList<String> tempList = LocationMap.get(firstChar);
+                if(tempList == null) {
+                    tempList = new ArrayList<>();
+                    tempList.add(line);
+                    //LocationMap.put(firstChar, tempList);
+                } else {
+                    tempList.add(line);
+                }
+            }
+           // System.out.println(countries);
+
+            locationReader.close();
+        } catch (IOException ioEx) {
+            System.err.println(ioEx.getMessage());
+        }
+    }*/
